ea68d89fefc2f046c184f4d65d5f50700cd235d5
Eshasonawane <142433608+Eshasonawane@users.noreply.github.com>
2026-07-17 16:48:08 +0530

    Time: 0 ms (100%), Space: 43.9 MB (88.8%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public static void solve (int[]nums,int index,List<Integer> output,List<List<Integer>> ans)
+    {
+        if(index>=nums.length)
+        {
+            //our subset is ready then store it;
+         ans.add(new ArrayList<>(output));
+         return;
+        }
+
+        int currentValue=nums[index];
+        //using include exclude
+        output.add(currentValue);
+        solve(nums,index+1,output,ans);
+        //remove the included element
+        output.remove(output.size()-1);
+        solve(nums,index+1,output,ans);
+    }
+    //backtracking and recursion
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>>ans =new ArrayList<>();
+        List<Integer>output = new ArrayList<>();
+        int index=0;
+         solve(nums,index,output,ans);
+         return ans;
+    }
+}
